a867be7f664135323db71358ab5ec12ef4fd3c0b
Diego Pérez <dperezrueda22@gmail.com>
2025-10-06 00:18:21 +0200

    Update GameManager.cs

--- Assets/Resources/Scripts/GameManager.cs
+++ Assets/Resources/Scripts/GameManager.cs
@@ -807,7 +807,6 @@ public class GameManager : MonoBehaviour
         {
             _nextRoundMenu.CloseMenu();
             _constructionMenu.CloseMenu();
-            _currencyIndicator.CloseMenu();
         }
         else if (_currentPhase == PhaseEnum.Combat)
         {
